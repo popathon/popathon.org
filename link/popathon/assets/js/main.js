51eed7baeb3f38de3e558dbcbe9b4772cce12652
Philo <philovankemenade@gmail.com>
2014-08-31 14:11:17 +0100

    all nav links corrected and added animation to button when scrolled into view

---
+++ link/popathon/assets/js/main.js
@@ -0,0 +1,26 @@
+;(function(){
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
+
+	// scroll into view elements
+	// attention seeking upcomming events button
+	$('#button-upcoming').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+	  if (isInView) {
+	    // element is now visible in the viewport
+	    if (visiblePartY == 'top') {
+	      // top part of element is visible
+	    } else if (visiblePartY == 'bottom') {
+	      // bottom part of element is visible
+	    } else {
+	      // whole part of element is visible
+	      $('#button-upcoming').addClass('animated tada');
+	    }
+	  } else {
+	    // element has gone out of viewport
+	  }
+	});
+})(jQuery)
